28172d1f16a4be12b78c82cb41e399f7095ae70c
unnamedAniki <67883330+unnamedAniki@users.noreply.github.com>
2023-02-27 21:56:05 +0300

    Update MapPage.xaml.cs

--- MAUtour/MapPage.xaml.cs
+++ MAUtour/MapPage.xaml.cs
@@ -3,6 +3,8 @@ using CommunityToolkit.Maui.Markup;
 using Mapsui;
 using Mapsui.Layers;
 using Mapsui.UI.Maui;
+using Mapsui.Rendering.Skia;
+using Microsoft.Maui.Animations;
 
 namespace MAUtour;
 
@@ -15,45 +17,84 @@ public partial class MapPage : ContentPage
         InitializeComponent();
 
         searchLabel = new Entry { Text = "Search something" };
-        var footerLaber = new Button { Text = "Add a pin" };
+        var addButton = new Button { Text = "Add a pin" };
+        var legendView = new ScrollView
+        {
+            Margin = new Thickness(20),
+            Content = GetView()
+        };
         view.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
-        footerLaber.Clicked += newPin;
-
+        addButton.Clicked += newPin;
+        view.PinClicked += View_SelectedPinChanged;
+        
         Content = new Grid
         {
             RowDefinitions =
             {
                 new RowDefinition { Height = new GridLength(20, GridUnitType.Auto)},
                 new RowDefinition { Height = new GridLength(20, GridUnitType.Star)},
-                new RowDefinition { Height = new GridLength(20, GridUnitType.Auto)}
+                new RowDefinition { Height = new GridLength(20, GridUnitType.Auto)},
+                new RowDefinition { Height = new GridLength(150, GridUnitType.Absolute)}
             },
             Children = 
             {
                 searchLabel.Row(0),
                 view.Row(1),
-                footerLaber.Row(2)
+                addButton.Row(2),
+                legendView.Row(3)
             }
         };
 
-        view.PinClicked += View_SelectedPinChanged;
     }
+    private View GetView()
+    {
+        DataTemplate dataTemplate = new DataTemplate(() =>
+        {
+            BoxView boxView = new BoxView
+            {
+                HeightRequest = 32,
+                WidthRequest = 32,
+                VerticalOptions = LayoutOptions.Center,
+                Margin = new Thickness(5),
+            };
+            boxView.SetBinding(BoxView.ColorProperty, "Color");
 
+            Label label = new Label
+            {
+                FontSize = 24,
+                TextColor = Color.FromArgb("#FFFFFF"),
+                VerticalOptions = LayoutOptions.Center,
+                Margin = new Thickness(5),
+            };
+            label.SetBinding(Label.TextProperty, "Label");
+
+            Label label1 = new Label
+            {
+                FontSize = 24,
+                TextColor = Color.FromArgb("#FFFFFF"),
+                VerticalOptions = LayoutOptions.Center,
+                Margin = new Thickness(5),
+            };
+            label1.SetBinding(Label.TextProperty, "Position");
+
+            StackLayout horizontalStackLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal
+            };
+            horizontalStackLayout.Add(boxView);
+            horizontalStackLayout.Add(label);
+            horizontalStackLayout.Add(label1);
+            return horizontalStackLayout;
+        });
+        StackLayout stackLayout = new StackLayout();
+        BindableLayout.SetItemsSource(stackLayout, view.Pins);
+        BindableLayout.SetItemTemplate(stackLayout, dataTemplate);
+        return stackLayout;
+    }
     private void newPin(object sender, EventArgs e)
     {
         var latRand = new Random(10);
-        var testPolyline = new Polyline()
-        {
-            Label = "testPolyline",
-            StrokeColor = Color.FromRgba(223, 33, 255, 255),
-            StrokeWidth = 3
-        };
-        var testPolygon = new Polygon()
-        {
-            Label = "testPolygon",
-            StrokeColor = Color.FromRgba(22, 40, 160, 255),
-            FillColor = Color.FromRgba(20, 40, 180, 128)
-        };
-        for (int i = 0; i < 1000; i++)
+        for (int i = 0; i < 5; i++)
         {
             var pin = new Pin()
             {
@@ -63,23 +104,11 @@ public partial class MapPage : ContentPage
                 MinVisible = 0.5,
                 Tag = "test",
                 Address = "TestAddress",
-                Color = Color.FromRgba(i*3,i,i*6,255)
+                Color = Color.FromRgba(i * 50, i, i * 60, 255)
             };
             view.Pins.Add(pin);
         }
-
-        testPolyline.Positions.Add(new Position(latRand.NextDouble() * 2, latRand.NextDouble() * 2));
-        testPolyline.Positions.Add(new Position(latRand.NextDouble() * 2, latRand.NextDouble() * 2));
-        testPolyline.Positions.Add(new Position(latRand.NextDouble() * 2, latRand.NextDouble() * 2));
-        testPolyline.Positions.Add(new Position(latRand.NextDouble() * 2, latRand.NextDouble() * 2));
-
-        testPolygon.Positions.Add(new Position(latRand.NextDouble() * 2, latRand.NextDouble() * 2));
-        testPolygon.Positions.Add(new Position(latRand.NextDouble() * 2, latRand.NextDouble() * 2));
-        testPolygon.Positions.Add(new Position(latRand.NextDouble() * 2, latRand.NextDouble() * 2));
-        testPolygon.Positions.Add(new Position(latRand.NextDouble() * 2, latRand.NextDouble() * 2));
-        testPolygon.Positions.Add(new Position(latRand.NextDouble() * 2, latRand.NextDouble() * 2));
-        view.Drawables.Add(testPolyline);
-        view.Drawables.Add(testPolygon);
+        
         view.RefreshData();
     }
 
