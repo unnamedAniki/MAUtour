bdf542c7f8923b0aa57ccab5195d63eda9a3f35c
unnamedAniki <67883330+unnamedAniki@users.noreply.github.com>
2023-03-13 21:12:29 +0300

    some refactoring

---
+++ MAUtour.Utils/DbConnect/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUtour.Utils.DbConnect
+{
+    internal class ApplicationContext
+    {
+    }
+}
